8335a1e9d0a904509b55e4210507a217d493d693
noman45569-lgtm <noman45569@gmail.com>
2026-07-17 20:44:43 +0530

    Delete app/src/main/cpp directory

--- app/src/main/cpp/native-lib.cpp
+++
@@ -1,7 +0,0 @@
-#include <jni.h>
-#include <string>
-
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_winlator_core_NativeLib_stringFromJNI(JNIEnv* env, jobject obj) {
-    return env->NewStringUTF("60FPS_Optimization_Active");
-}
